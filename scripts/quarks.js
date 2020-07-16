54326142e353b2aaa46db0d1ead549c1b4c4d7e6
lukaaa-m <lukablueowl51@gmail.com>
2020-07-16 10:04:12 +1000

    Create quark class & quark types in JSON

---
+++ scripts/quarks.js
@@ -0,0 +1,12 @@
+class Quark{
+    constructor(name, colour, mass, charge, spin){
+        this.name = name;
+        this.colour = colour;
+        this.mass = mass;
+        this.charge = charge;
+        this.spin = spin;
+    }
+}
+
+
+
