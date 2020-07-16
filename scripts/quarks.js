7f6d66e77a9233c8b288a4bb5cfa16b93a8eecfe
lukaaa-m <lukablueowl51@gmail.com>
2020-07-16 14:19:26 +1000

    Setting up displaying of quark properties on webpage

--- scripts/quarks.js
+++ scripts/quarks.js
@@ -6,7 +6,24 @@ class Quark{
         this.charge = charge;
         this.spin = spin;
     }
-}
+};
 
+$.getJSON('http://localhost:8888/scripts/quarks.json', function(data) {
+
+    var text = JSON.stringify(data)
+    console.log(text)
+    $(".quarkdump").innerHTML(text);
+});
+
+
+
+//var quarksJSON = $.getJSON('http://localhost:8888/scripts/quarks.json', function(data){
+//    console.log('success');
+//    testdump.innerHTML = data;
+//});
+
+//for quark in quarksJSON:
+//  for colour in quark.colours:
+//      quarks.append(Quark(*properties))
 
 
