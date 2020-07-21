edc2c71b152ef52fb382dc1a76a16dc84c7bcbdb
lukaaa-m <lukablueowl51@gmail.com>
2020-07-21 15:06:45 +1000

    Set up trapezoids, rotate and scale functions (I broke them but eh)

--- scripts/quarks.js
+++ scripts/quarks.js
@@ -1,29 +1,96 @@
-class Quark{
-    constructor(name, colour, mass, charge, spin){
-        this.name = name;
-        this.colour = colour;
-        this.mass = mass;
-        this.charge = charge;
-        this.spin = spin;
-    }
+quarks = {"up": {
+		"name": "up",
+		"colours": ["red","green","blue"],
+		"mass": "2.2",
+		"charge": "2/3",
+		"spin": "1/2",
+	},
+	
+	"down": {
+		"name": "down",
+		"colours": ["red","green","blue"],
+		"mass": "4.7",
+		"charge": "-1/3",
+		"spin": "1/2",
+	},
+	
+	"charm": {
+		"name": "charm",
+		"colours": ["red","green","blue"],
+		"mass": "1280",
+		"charge": "2/3",
+		"spin": "1/2",
+	},
+	
+	"strange": {
+		"name": "strange",
+		"colours": ["red","green","blue"],
+		"mass": "96",
+		"charge": "-1/3",
+		"spin": "1/2",
+	},
+	
+	"top": {
+		"name": "top",
+		"colours": ["red","green","blue"],
+		"mass": "173100",
+		"charge": "2/3",
+		"spin": "1/2",
+	},
+	
+	"bottom": {
+		"name": "bottom",
+		"colours": ["red","green","blue"],
+		"mass": "4180",
+		"charge": "-1/3",
+		"spin": "1/2",
+	},
+	
+	"anti-up": {
+		"name": "anti-up",
+		"colours": ["anti-red","anti-green","anti-blue"],
+		"mass": "2.2",
+		"charge": "-2/3",
+		"spin": "1/2",
+	},
+	
+	"anti-down": {
+		"name": "anti-down",
+		"colours": ["anti-red","anti-green","anti-blue"],
+		"mass": "4.7",
+		"charge": "1/3",
+		"spin": "1/2",
+	},
+	
+	"anti-charm": {
+		"name": "anti-charm",
+		"colours": ["anti-red","anti-green","anti-blue"],
+		"mass": "1280",
+		"charge": "-2/3",
+		"spin": "1/2",
+	},
+	
+	"anti-strange": {
+		"name": "anti-strange",
+		"colours": ["anti-red","anti-green","anti-blue"],
+		"mass": "96",
+		"charge": "1/3",
+		"spin": "1/2",
+	},
+	
+	"anti-top": {
+		"name": "anti-top",
+		"colours": ["anti-red","anti-green","anti-blue"],
+		"mass": "173100",
+		"charge": "-2/3",
+		"spin": "1/2",
+	},
+	
+	"anti-bottom": {
+		"name": "anti-bottom",
+		"colours": ["anti-red","anti-green","anti-blue"],
+		"mass": "4180",
+		"charge": "1/3",
+		"spin": "1/2",
+    },
 };
-
-$.getJSON('http://localhost:8888/scripts/quarks.json', function(data) {
-
-    var text = JSON.stringify(data)
-    console.log(text)
-    $(".quarkdump").innerHTML(text);
-});
-
-
-
-//var quarksJSON = $.getJSON('http://localhost:8888/scripts/quarks.json', function(data){
-//    console.log('success');
-//    testdump.innerHTML = data;
-//});
-
-//for quark in quarksJSON:
-//  for colour in quark.colours:
-//      quarks.append(Quark(*properties))
-
-
